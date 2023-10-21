42e391845f282aa2c8fa792ccd47be182acecfea
tetsu633 <tetsunishimura633@gmail.com>
2023-10-21 12:07:00 +0900

    1.コンポーズ可能な関数

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -2,9 +2,27 @@ package com.example.jetpackcomposetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent{
+            MessageCard(name = "World")
+        }
+    }
+
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(name = "Android")
     }
 }
